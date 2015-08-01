9b989e6f76a13af16c10ea2fd689cec826b562e3
Daniel Husar <dano.husar@gmail.com>
2015-08-01 17:51:00 +0200

    indentation

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 var minimatch = require('minimatch');
 
 function timestamp (opts) {
-  var opts = opts || {};
+	var opts = opts || {};
 
-  return function (files, metalsmith, done) {
-  	var data;
-    Object.keys(files).filter(minimatch.filter('*.html', { matchBase: true })).forEach(function (file, index, arr) {
-    	data = files[file];
-    	data.contents = new Buffer(data.contents.toString() + '\r\n<!-- Genarated on: '+ new Date() +' -->');
-    });
-    done();
-  }
+	return function (files, metalsmith, done) {
+		var data;
+		Object.keys(files).filter(minimatch.filter('*.html', { matchBase: true })).forEach(function (file, index, arr) {
+			data = files[file];
+			data.contents = new Buffer(data.contents.toString() + '\r\n<!-- Genarated on: '+ new Date() +' -->');
+		});
+		done();
+	}
 }
 
 module.exports = timestamp;
--- test/index.js
+++ test/index.js
@@ -6,21 +6,21 @@ var timestamp = require('../');
 
 it('should add timestamp to html file', function (cb) {
 	function test () {
-	  return function (files, metalsmith, done) {
-	  	var file = files['index.html'].contents.toString();
-	  	assert(!!file.match(/<!-- Genarated on:/));
-	  	done();
-	  }
+		return function (files, metalsmith, done) {
+			var file = files['index.html'].contents.toString();
+			assert(!!file.match(/<!-- Genarated on:/));
+			done();
+		}
 	}
 
-  Metalsmith('test/fixtures/html')
-    .use(timestamp())
-    .use(test())
-    .build(function (err) {
-    	if (err) {
-    		throw err;
-    	}
-    	cb();
-    });
+	Metalsmith('test/fixtures/html')
+	.use(timestamp())
+	.use(test())
+	.build(function (err) {
+		if (err) {
+			throw err;
+		}
+		cb();
+	});
 });
 
